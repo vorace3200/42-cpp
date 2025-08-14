4e151c256b5a00e8b0de05cba364fb2d82d482cd
vorace32 <vorace32000@gmail.com>
2025-08-14 06:39:14 +0200

    Ex02 | Module1

---
+++ Module1/Ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "\n str addr : " << &str << std::endl;
+    std::cout << "stringPTR addr : " << stringPTR << std::endl;
+    std::cout << "stringREF addr : " << &stringREF << std::endl;
+
+    std::cout << "\n ============================== \n " << std::endl;
+
+    std::cout << "str value : " << str << std::endl;
+    std::cout << "stringPTR value : " <<  *stringPTR << std::endl;
+    std::cout << "stringREF value : " <<  stringREF << std::endl;
+
+
+    return 0;
+}
