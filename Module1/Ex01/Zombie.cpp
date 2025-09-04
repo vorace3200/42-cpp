08a8047e6d9b2c915ac8fa0a5ef77999268f6a6c
vorace32 <vorace32000@gmail.com>
2025-09-04 14:11:46 +0200

    Ex01 | Module1 changed

--- Module1/Ex01/Zombie.cpp
+++ Module1/Ex01/Zombie.cpp
@@ -1,15 +1,23 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie() : name("")
+{
+
+}
+
+Zombie::~Zombie()
+{
+    std::cout << this->name << " has been destroyed." << std::endl;
+}
+
+Zombie::Zombie(const std::string& name) : name(name)
+{
+
+}
 
 void Zombie::setName(std::string name)
 {
     this->name = name;
-}
-Zombie* Zombie::NewZombie(std::string name)
-{
-    Zombie* zombie = new Zombie();
-    zombie->name = name;
-    return zombie;
 }
 
 void Zombie::announce()
