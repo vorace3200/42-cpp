c89638750d43a3e0abcabe34f5f8df759585a2ed
vorace32 <vorace32000@gmail.com>
2025-10-07 13:54:19 +0200

    better main

--- Module4/Ex01/main.cpp
+++ Module4/Ex01/main.cpp
@@ -15,23 +15,29 @@ int main()
     delete i;
     
     std::cout << std::endl;
-    std::cout << "==== Array test ====" << std::endl;
+    std::cout << "======== Array test ========" << std::endl;
     
     Animal* animals[4];
     
+    std::cout << "---DOG---" << std::endl;
     for (int idx = 0; idx < 2; idx++)
         animals[idx] = new Dog();
+        
+    std::cout << "---CAT---" << std::endl;
     for (int idx = 2; idx < 4; idx++)
         animals[idx] = new Cat();
-        
+
+    std::cout << "---DELETE---" << std::endl;
     for (int idx = 0; idx < 4; idx++)
         delete animals[idx];
         
-    std::cout << std::endl;
-    std::cout << "==== Deep copy test ====" << std::endl;
     
-    Dog basic;
-    Dog tmp = basic;
+    std::cout << std::endl; std::cout << "======== Copy test ========" << std::endl;
+    
+    Dog HeGotanIdea;
+    HeGotanIdea.setIdea(1, "test hello");
+    Dog HeStealTheIdea = HeGotanIdea;
+    std::cout << "Copy idea : " << HeStealTheIdea.getIdea(1) << std::endl;
     
     return 0;
 }
